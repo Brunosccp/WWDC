26320cd15b7feaf4b22177a99ec4b29b8824f1f2
Bruno Rocca <bruno@bruno-diego-souza-idolo.local>
2018-03-31 20:20:58 -0300

    botões de compasso criados

--- WWDC.playground/Contents.swift
+++ WWDC.playground/Contents.swift
@@ -12,18 +12,38 @@ public class TestViewController : UIViewController {
     public override func loadView() {
         let view = UIView()
         view.backgroundColor = .orange
+        
+        //creating buttons
         let playButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        let nextCompass = UIButton(frame: CGRect(x: 375 - 50, y: 0, width: 50, height: 50))
+        let previousCompass = UIButton(frame: CGRect(x: 375 - 100, y: 0, width: 50, height: 50))
+        
         playButton.backgroundColor = .blue
+        nextCompass.backgroundColor = .black
+        previousCompass.backgroundColor = .gray
         
         view.addSubview(playButton)
+        view.addSubview(nextCompass)
+        view.addSubview(previousCompass)
         
         playButton.addTarget(self, action: #selector(playAction), for: .touchUpInside)
+        nextCompass.addTarget(self, action: #selector(nextCompassAction), for: .touchUpInside)
+        previousCompass.addTarget(self, action: #selector(previousCompassAction), for: .touchUpInside)
+        
+        //creating compass buttons
+        
         self.view = view
 
     }
     @objc func playAction(){
         print("foi apertado")
         demoView.startTimer()
+    }
+    @objc func nextCompassAction(){
+        print("compasso++")
+    }
+    @objc func previousCompassAction(){
+        print("compasso--")
     }
     
     public override func viewDidAppear(_ animated: Bool) {
@@ -32,7 +52,7 @@ public class TestViewController : UIViewController {
         let width: CGFloat = self.view.frame.size.width
         let height: CGFloat = self.view.frame.size.height
 
-        demoView = DemoView(frame: CGRect(x: 0, y: 668/15,
+        demoView = DemoView(frame: CGRect(x: 0, y: height/15,
                                                      width: 375,
                                                      height: 623.466))
         
@@ -40,10 +60,6 @@ public class TestViewController : UIViewController {
 
         
         self.view.addSubview(demoView)
-    }
-    @objc func updateDemoView(){
-        
-        
     }
 
 
